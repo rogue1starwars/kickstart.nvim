c8d609bb07e8a994096f0da03a0b073ef747ac6c
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-07-25 21:23:32 +0900

    Modified esc keymap

--- init.lua
+++ init.lua
@@ -187,6 +187,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Set esc to jj
+vim.keymap.set('i', 'jj', '<Esc>')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
