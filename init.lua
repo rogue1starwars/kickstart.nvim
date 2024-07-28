8e303323419724a5221ada8a928493472508d4d0
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-07-28 20:44:04 +0900

    fixed conflicts again!

--- init.lua
+++ init.lua
@@ -651,19 +651,11 @@ require('lazy').setup {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-<<<<<<< HEAD
         'stylua', -- Used to format Lua code
         'prettier', -- Used to format JavaScript, TypeScript, etc.
         'isort', -- Used to format Python code
         'black', -- Used to format Python code
         'pylint', -- Used to lint Python code
-=======
-        'prettier', -- Used to format JavaScript, TypeScript, etc.
-        'stylua',   -- Used to format Lua code
-        'isort',    -- Used to format Python code
-        'black',    -- Used to format Python code
-        'pylint',   -- Used to lint Python code
->>>>>>> af7adfc2003f4b20a2342e213d75cfca8a91f9cd
         'eslint_d', -- Used to lint JavaScript, TypeScript, etc.
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
@@ -993,7 +985,6 @@ require('lazy').setup {
     end,
   },
 
-<<<<<<< HEAD
   -- function CopilotChatBuffer()
   --   local input = vim.fn.input("Quick Chat: ")
   --   if input ~= "" then
@@ -1020,8 +1011,6 @@ require('lazy').setup {
       -- See Configuration section for rest
     },
     -- See Commands section for default commands if you want to lazy load on them
-=======
->>>>>>> af7adfc2003f4b20a2342e213d75cfca8a91f9cd
 
     vim.keymap.set('n', '<leader>cco', function()
       require('CopilotChat').open()
@@ -1079,12 +1068,7 @@ require('lazy').setup {
       }
 
       -- set keymaps
-<<<<<<< HEAD
       local keymap = vim.keymap -- for conciseness
-=======
-      local keymap = vim
-          .keymap                                                                                                         -- for conciseness
->>>>>>> af7adfc2003f4b20a2342e213d75cfca8a91f9cd
 
       keymap.set('n', '<leader>ee', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle file explorer' }) -- toggle file explorer
       keymap.set('n', '<leader>ef', '<cmd>NvimTreeFindFileToggle<CR>', { desc = 'Toggle file explorer on current file' }) -- toggle file explorer on current file
